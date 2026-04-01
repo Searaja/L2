732eff0afdfdf84f5749cb71cb2937003160ddeb
Ramon Igarreta <ramon@fbmc.fcen.uba.ar>
2026-04-01 11:33:30 -0300

    Database correction and better script readability

--- repo/script.R
+++ repo/script.R
@@ -1,3 +1,5 @@
+# LIBRARIES
+
 library(modelsummary)
 library(car)
 library(lme4)
@@ -9,7 +11,9 @@ library(ggplot2)
 library(DHARMa)
 library(nlme)
 
-df <- read.csv("behavioral.csv", header = TRUE) 
+# BEHAVIORAL DATA
+
+df <- read.csv("data/behavioral.csv", header = TRUE) 
 
 df$subject           <- factor(df$file)
 df$word_type           <- factor(df$categ)
@@ -23,16 +27,13 @@ levels(df$relatedness)[levels(df$relatedness) == "right"] <- "Related"
 levels(df$relatedness)[levels(df$relatedness) == "left"] <- "Unrelated"
 df$word_type <- factor(df$word_type,
                        levels = c("L1", "L2-Remote", "L2-Recent"))
-# 1
 
-df_filtrado <- df#  %>% filter(word_type != "L2-Recent")
-
-# Acc
-# Modelo lineal mixto generalizado con interacción
+# Accuracy
+# GLMM
 
 model_Acc <- glmer(
   accuracy ~ word_type * relatedness +
-    (1 | subject) + (1 | item), data = df_filtrado, family = binomial)
+    (1 | subject) + (1 | item), data = df, family = binomial)
 
 summary(model_Acc)
 
@@ -51,78 +52,78 @@ p <- emmip(model_Acc, relatedness ~ word_type)
 print(p + ggplot2::ggtitle("Accuracy"))
 emmeans(model_Acc, pairwise ~ word_type , type = "response")
 emmeans(model_Acc, pairwise ~ word_type | relatedness, type = "response")
-pairs(emmeans(model_Acc, ~ relatedness | word_type))
+pairs(emmeans(model_Acc, ~ relatedness | word_type, type = "response"))
 
 # RT
-# Modelo lineal mixto con interacción. Solo correctas y con SD 2.5
+# LMM - log-transformed
+# Only correct answers and < 2.5 SD from mean
 
-df_filtrado2 <- df_filtrado %>%
+k <- 2.5
+
+df_filt <- df %>%
   group_by(corrAns, categ) %>%
-  filter(abs(rt - mean(rt, na.rm = TRUE)) < 2.5 * sd(rt, na.rm = TRUE)) %>%
+  filter(abs(rt - mean(rt, na.rm = TRUE)) < k * sd(rt, na.rm = TRUE)) %>%
   ungroup()
 
 model_RT <- lme( log(rt) ~ word_type * relatedness 
-      ,random = ~1 | subject/item, data = df_filtrado2, weights = varIdent(form = ~1 | word_type))
+      ,random = ~1 | subject/item, data = df_filt, weights = varIdent(form = ~1 | word_type))
 
 summary(model_RT)
 
+res <- residuals(model_RT)
+qqnorm(res)
+qqline(res)
+
+plot(fitted(model_RT), res)
+abline(h = 0)
+
+
 Anova(model_RT, type="II")
 
-#qqnorm(resid(model_RT, type = "normalized"))
-#qqline(resid(model_RT, type = "normalized"))
-
-#p <- emmip(model_RT,  word_type ~ relatedness,lmerTest.limit = 5726,pbkrtest.limit = 5726)
-#print(p + ggplot2::ggtitle("RT"))
+p <- emmip(model_RT, relatedness ~ word_type)
+print(p + ggplot2::ggtitle("RT"))
 emmeans(model_RT, pairwise ~ word_type, type="response")
 emmeans(model_RT, pairwise ~ relatedness, type="response")
 
 
-datos <- read.csv("epoch_amplitudes.csv", header = TRUE)
+# EEG DATA
 
-datos$subject <- as.factor(datos$subject)
-datos$item <- as.factor(datos$item)
-datos$relatedness <- as.factor(datos$relatedness)
-datos$word_type <- as.factor(datos$language)
-datos$channel <- as.factor(datos$channel)
+data <- read.csv("data/epoch_amplitudes.csv", header = TRUE)
 
-datos <- datos %>%
+data$subject <- as.factor(data$subject)
+data$item <- as.factor(data$item)
+data$relatedness <- as.factor(data$relatedness)
+data$word_type <- as.factor(data$language)
+data$channel <- as.factor(data$channel)
+
+levels(data$word_type)[levels(data$word_type) == "rem"] <- "L2-Remote"
+levels(data$word_type)[levels(data$word_type) == "rec"] <- "L2-Recent"
+levels(data$word_type)[levels(data$word_type) == "esp"] <- "L1"
+levels(data$relatedness)[levels(data$relatedness) == 1] <- "Related"
+levels(data$relatedness)[levels(data$relatedness) == 0] <- "Unrelated"
+
+
+# CHANGE window_start AND channel TO SEE RESULTS FOR DIFFERENT
+# TIME WINDOWS AND ELECTRODE SITES:
+# 0.3 FOR N400 (300-500 ms); 0.5 FOR LPC (500-800 ms)
+# LMM
+# Only correct answers and RT < 2.5 SD from RT mean
+
+data_filt <- data %>%
   filter(window_start == 0.3, channel == "Cz")
 
-datos_filtrados <- datos %>%
-  filter(subject!="CB", subject!="DK", subject!="EDA", subject!="MGM", subject!="RAC")
-
-datos_filtrados <- datos_filtrados %>%
+data_filt <- data_filt %>%
   group_by(subject,word_type,relatedness) %>%
   mutate(
     media_rt = mean(RT, na.rm = TRUE),
     sd_rt = sd(RT, na.rm = TRUE)
   ) %>%
-  filter(RT > media_rt - 2.5*sd_rt,     # if SD filter changed here,
-         RT < media_rt + 2.5*sd_rt) %>% # change in behavior too!
+  filter(RT > media_rt - k*sd_rt,     
+         RT < media_rt + k*sd_rt) %>% 
   ungroup()
 
-resumen_por_sujeto <- datos_filtrados %>%
-  group_by(subject, word_type, relatedness) %>%
-  summarise(
-    n_trials = n(), # Cuántos quedaron para este sujeto en esta condición
-    .groups = "drop"
-  )
 
-# Ahora calculamos la MEDIA y el SD de esos conteos
-reporte_final <- resumen_por_sujeto %>%
-  group_by(word_type, relatedness) %>%
-  summarise(
-    media_trials = mean(n_trials),
-    sd_trials = sd(n_trials),
-    min_trials = min(n_trials), # Útil para ver el "peor" caso
-    max_trials = max(n_trials),
-    n_sujetos = n(),
-    .groups = "drop"
-  )
-
-print(reporte_final)
-
-reporte_robusto <- datos_filtrados %>%
+report <- data_filt %>%
   group_by(subject, word_type, relatedness) %>%
   summarise(n_trials = n(), .groups = "drop") %>%
   group_by(word_type, relatedness) %>%
@@ -137,69 +138,46 @@ reporte_robusto <- datos_filtrados %>%
     .groups = "drop"
   )
 
-print(reporte_robusto)
+print(report)
 
-ggplot(resumen_por_sujeto, aes(x = factor(subject), y = n_trials, fill = relatedness)) +
-  geom_bar(stat = "identity", position = "dodge") +
-  facet_wrap(~word_type) +
-  theme_minimal() +
-  labs(title = "Trials restantes por sujeto y condición",
-       x = "Sujeto", y = "Número de Trials") +
-  theme(axis.text.x = element_text(angle = 45))
+#ggplot(data_filt, aes(x = subject, y = mean_amplitude)) +
+#  geom_boxplot() +
+#  theme_minimal()
 
-ggplot(datos_filtrados, aes(x = subject, y = mean_amplitude)) +
-  geom_boxplot() +
-  theme_minimal()
-
-datos_filtrados <- datos_filtrados %>%
+data_filt <- data_filt %>%
   select(mean_amplitude , word_type , relatedness, subject, item) %>%
   na.omit()
 
-modelo <- glmmTMB(
+model_eeg <- glmmTMB(
   mean_amplitude ~ word_type * relatedness +
     (1 | subject) + (1 | item),
   dispformula = ~ word_type*relatedness,
-  data = datos_filtrados
+  data = data_filt
 )
 
-#infl <- influence(modelo, group = "subject")
+summary(model_eeg)
 
-#plot(infl, which = "cook")
+Anova(model_eeg, type="II")
 
-summary(modelo)
+hist(data_filt$mean_amplitude, breaks = 100)
 
-Anova(modelo, type="III")
-
-hist(datos_filtrados$mean_amplitude, breaks = 100)
-
-sim_res <- simulateResiduals(modelo)
+sim_res <- simulateResiduals(model_eeg)
 plot(sim_res)
 testUniformity(sim_res)
-#check_model(modelo)
 
-emm <- emmeans(modelo, ~ word_type * relatedness)
-emm
-
-pairs(emm)
-
+emm <- emmeans(model_eeg, ~ word_type * relatedness)
 plot(emm)
 
-# 1. Calcular las medias por grupo
-emmeans(modelo, pairwise ~ word_type, adjust = "tukey")
-emmeans(modelo, pairwise ~ relatedness, adjust = "tukey")
+emmeans(model_eeg, pairwise ~ word_type, adjust = "tukey")
+emmeans(model_eeg, pairwise ~ relatedness, adjust = "tukey")
 
-emmeans(modelo, pairwise ~ word_type|relatedness, adjust = "tukey")
-emmeans(modelo, pairwise ~ relatedness|word_type, adjust = "tukey")
+emmeans(model_eeg, pairwise ~ word_type|relatedness, adjust = "tukey")
+emmeans(model_eeg, pairwise ~ relatedness|word_type, adjust = "tukey")
 
-# 2. Obtienes las comparaciones de a pares
-#comparaciones <- pairs(medias_ajustadas, adjust = "bonferroni")
-#print(comparaciones)
-
-res <- residuals(modelo)
+res <- residuals(model_eeg)
 
 qqnorm(res)
 qqline(res)
-# shapiro.test(res)
 
-plot(fitted(modelo), res)
+plot(fitted(model_eeg), res)
 abline(h = 0)
